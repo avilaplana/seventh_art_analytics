524df99847b0fa68ff4996f282e25cc6e01b9e6a
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmaill.com>
2026-01-09 17:11:09 +0100

    Use s3a in Airflow Dag Pyspark job

--- airflow/dags/extract_load_dag.py
+++ airflow/dags/extract_load_dag.py
@@ -67,17 +67,22 @@ with DAG(
         --master spark://spark-iceberg:7077 \\
         --deploy-mode client \\
         --conf spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions \\
-        --conf spark.sql.catalog.demo=org.apache.iceberg.spark.SparkCatalog \\
-        --conf spark.sql.catalog.demo.type=rest \\
-        --conf spark.sql.catalog.demo.uri=http://rest:8181 \\
-        --conf spark.sql.catalog.demo.io-impl=org.apache.iceberg.hadoop.HadoopFileIO \\
-        --conf spark.sql.catalog.demo.warehouse=s3a://warehouse/ \\
         --conf spark.sql.defaultCatalog=demo \\
+        --conf spark.sql.catalog.demo=org.apache.iceberg.spark.SparkCatalog \\
+        --conf spark.sql.catalog.demo.catalog-impl=org.apache.iceberg.rest.RESTCatalog \\
+        --conf spark.sql.catalog.demo.uri=http://rest:8181 \\
+        --conf spark.sql.catalog.demo.warehouse=s3://warehouse/ \\
+        --conf spark.sql.catalog.demo.io-impl=org.apache.iceberg.aws.s3.S3FileIO \\
+        --conf spark.sql.catalog.demo.s3.endpoint=http://minio:9000 \\
+        --conf spark.sql.catalog.demo.s3.path-style-access=true \\
+        --conf spark.sql.catalog.demo.s3.access-key-id=admin \\
+        --conf spark.sql.catalog.demo.s3.secret-access-key=password \\
         --conf spark.hadoop.fs.s3a.endpoint=http://minio:9000 \\
-        --conf spark.sql.s3a.path.style.access=true \\
+        --conf spark.hadoop.fs.s3a.path.style.access=true \\
         --conf spark.hadoop.fs.s3a.access.key=admin \\
         --conf spark.hadoop.fs.s3a.secret.key=password \\
-        --conf spark.hadoop.fs.s3.impl=org.apache.hadoop.fs.s3a.S3AFileSystem \\
+        --conf spark.driver.extraJavaOptions="-Daws.region=eu-west-2" \\
+        --conf spark.executor.extraJavaOptions="-Daws.region=eu-west-2" \\
         --packages org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.8.1,org.apache.iceberg:iceberg-aws:1.8.1,org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262 \\
         {SPARK_JOBS_DIR}{job}.py""",  
         )
