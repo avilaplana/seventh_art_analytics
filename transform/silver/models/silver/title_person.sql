af12458a07cec0d2231cc14e96109f51bb0562e7
Alvaro Vilaplana Garcia <alvaro.vilaplana@gmaill.com>
2026-02-03 15:13:36 +0000

    Fix DBT transformation for demo.silver.title_person

--- transform/silver/models/silver/title_person.sql
+++ transform/silver/models/silver/title_person.sql
@@ -1,9 +1,42 @@
-SELECT 
-tconst AS title_id,
-nconst AS person_id,
-ordering, 
-CASE
-     WHEN characters = '\\N' THEN NULL
-    ELSE SPLIT(TRIM(REPLACE(REPLACE(REPLACE(characters, '[', ''), ']', ''), '\"','')), ',')
-END AS characters
-FROM {{ source('bronze', 'title_principals') }}
+WITH title_principals_cleaned AS (
+  SELECT
+    tconst AS title_id,
+    nconst AS person_id,
+    category,
+    ordering,
+    CASE
+      WHEN job = '\\N' THEN NULL
+      ELSE job
+    END AS job,
+    CASE
+      WHEN characters = '\\N' THEN 'NOT DEFINED'
+      ELSE TRIM(REPLACE(REPLACE(REPLACE(characters, '[', ''), ']', ''), '\"',''))
+    END AS characters
+FROM {{ source ('bronze', 'title_principals') }}
+),
+title_principals_with_map AS (
+SELECT
+    title_id,
+    person_id,
+    category,
+    job,
+    COUNT(*) AS number_of_roles,
+    map_from_entries(collect_set(struct(ordering, characters))) AS characters
+FROM title_principals_cleaned
+GROUP BY
+    title_id,
+    person_id,
+    category,
+    job
+)
+
+SELECT
+    tpwm.title_id,
+    tpwm.person_id,
+    r.role_id,
+    tpwm.job,
+    tpwm.number_of_roles,
+    tpwm.characters
+FROM title_principals_with_map tpwm
+JOIN {{ ref('role') }} r
+ON tpwm.category = r.role_name
--- transform/silver/models/silver/title_person_role.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-  r.role_id, 
-  tconst AS title_id, 
-  nconst AS person_id
-FROM {{ source('bronze', 'title_principals') }} tp
-JOIN {{ ref('role') }} r
-ON tp.category = r.role_name
